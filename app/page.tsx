87304965edacc6286cf654947724d5db7e26066b
Agnes Pilar <agnes.pilar@yahoo.de>
2023-02-14 12:23:42 +0100

    Add graphql server request

---
+++ app/page.tsx
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+import Image from 'next/image';
+import { initializeApollo } from '../util/client';
+
+type GitHubProfileResponse = {
+  user: {
+    name: string;
+    avatarUrl: string;
+    repositories: {
+      edges: {
+        node: {
+          name: string;
+          id: string;
+        };
+      }[];
+    };
+  };
+};
+
+export default async function HomePage() {
+  const client = initializeApollo(null);
+
+  const { data } = await client.query<GitHubProfileResponse>({
+    query: gql`
+      query profileQuery($username: String = "agilucia") {
+        user(login: $username) {
+          name
+          avatarUrl
+          repositories(last: 10) {
+            edges {
+              node {
+                name
+                defaultBranchRef {
+                  name
+                  id
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  console.log(data);
+
+  return (
+    <main>
+      <h1>{data.user.name}'s Profile</h1>
+      <Image
+        src={data.user.avatarUrl}
+        alt={`${data.user.name}'s avatar`}
+        width="200"
+        height="200"
+      />
+      <h2>Repositories</h2>
+      {data.user.repositories.edges.map((repository) => (
+        <li key={repository.node.id}>{repository.node.name}</li>
+      ))}
+    </main>
+  );
+}
